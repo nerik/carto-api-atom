04efa742be622ed36235cac22e7e46fbbc08dac2
nerik <erik.escoffier@gmail.com>
2016-07-17 19:30:32 +0200

    cleanup

--- spec/carto-api-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import CartoApiView from '../lib/carto-api-view';
-
-describe('CartoApiView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
